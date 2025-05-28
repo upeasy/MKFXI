d36323168e8c3a18fe3c40328e04da1b2ad7d62f
upeasy <upeasybd@gmail.com>
2025-05-28 19:09:16 +0600

    update

--- components/ImageUploader.tsx
+++ components/ImageUploader.tsx
@@ -11,7 +11,7 @@ export const ImageUploader = ({ label, inputId, onChange }: Props) => {
   return (
     <div
       onClick={() => document.getElementById(inputId)?.click()}
-      className="absolute top-1/2 left-1/2 transform -translate-x-1/2 w-full h-full -translate-y-1/2 rounded-full flex justify-center items-center cursor-pointer"
+      className="absolute top-1/2 left-1/2 transform -translate-x-1/2 w-full h-full -translate-y-1/2 rounded-full flex justify-center items-center cursor-pointer text-primary"
     >
       <input
         id={inputId}
