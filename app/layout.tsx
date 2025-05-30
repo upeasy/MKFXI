66367f6afe7fe8b19e636ba25476fd9aa8113e98
upeasy <upeasybd@gmail.com>
2025-05-30 10:57:52 +0600

    pwa build

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,21 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "MKFXI | Matbor Kandi Football Ekadosh",
   description: "Matbor Kandi Football Ekadosh - MKFXI",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  authors: [
+    {
+      name: "Nazmul H. Sourab",
+      url: "https://www.linkedin.com/in/mdnhs/",
+    },
+  ],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "/logo.png" },
+    { rel: "icon", url: "/logo.png" },
+  ],
 };
 
 export default function RootLayout({
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import withPWA from "next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development",
+  },
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+})(nextConfig);
