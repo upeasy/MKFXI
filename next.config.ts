3d02cafce851cca3386ee8e6511a9959d3e29d9b
UpEasy <upeasybd@gmail.com>
2025-08-01 22:37:07 +0600

    build fix

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,7 @@
-import withPWA from "next-pwa";
-
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  compiler: {
-    removeConsole: process.env.NODE_ENV !== "development",
-  },
+
 };
 
-export default withPWA({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
-})(nextConfig);
+export default nextConfig;
